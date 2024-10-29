66087118526c59ef826cd07cf76ab3c094719d98
Katherine Whitmoyer <katewhitmoyer@gmail.com>
2024-10-28 23:25:11 -0400

    Basic privacy screen functionality

--- state_manager.py
+++ state_manager.py
@@ -1,4 +1,5 @@
 import pygame as py 
+from constants_config import *
 
 
 class Game(object):
@@ -14,11 +15,28 @@ class Game(object):
         self.states = states
         self.state_name = start_state
         self.state = self.states[self.state_name]
+        self.idle_screen = False
+        self.idle_timer = 0
+
+        
+        self.start_button = StartButton(box_x_position=300, box_y_position=550)
 
     def event_loop(self):
         #processes events and passes them to the current state 
         for event in py.event.get():
-            self.state.get_event(event)
+            if self.idle_screen:
+                if event.type == py.QUIT:
+                    self.done = True
+                elif event.type == py.MOUSEBUTTONDOWN:
+                    mouse_position = py.mouse.get_pos()
+                    if self.start_button.check_start_button_click(mouse_position):
+                        self.idle_screen = False
+                        self.idle_timer = 0
+            else:
+                if event.type in (py.MOUSEMOTION, py.MOUSEBUTTONDOWN, py.KEYDOWN):
+                    self.idle_timer = 0
+                self.state.get_event(event)
+
 
     def flip_state(self):
         #actually switches to the next game state and passes persistant data to the new state
@@ -32,6 +50,12 @@ class Game(object):
 
     def update(self, dt):
         #checks for state flip and updates the current active state
+        self.idle_timer = self.idle_timer + (dt / 1000)
+        print(f"Idle Timer: {self.idle_timer:.2f} seconds")
+
+        if self.idle_timer > TIME_TO_TIMEOUT:
+            self.idle_screen = True
+
         if self.state.quit:
             self.done = True
         elif self.state.done:
@@ -41,7 +65,14 @@ class Game(object):
     def draw(self):
         #draws the current state to the screen
         self.state.draw(self.screen)
-        
+
+        if self.idle_screen:
+            idle_screen_color = WARM_SOFT_WHITE
+            idle_screen_rect = self.screen.get_rect()
+            py.draw.rect(self.screen, idle_screen_color, idle_screen_rect)
+
+            self.start_button.render(self.screen)
+
     def run(self):
         #main game loop
         #Controls updating the game, processing events, and drawing the screen 
@@ -51,3 +82,27 @@ class Game(object):
             self.update(dt)
             self.draw()
             py.display.update()
+
+class StartButton:
+    def __init__(self, box_x_position=152, box_y_position=1200, info_box_width=400, info_box_height=67):
+        self.info_box_width = info_box_width
+        self.info_box_height = info_box_height
+        self.box_x_position = box_x_position
+        self.box_y_position = box_y_position
+        self.font = py.font.SysFont('georgia', 36)
+
+    def render(self, surface):
+        button_rect = py.Rect(self.box_x_position, self.box_y_position, self.info_box_width, self.info_box_height)
+        button_color = MEDIUM_GREEN 
+        py.draw.rect(surface, button_color, button_rect, border_radius=20)
+        
+        button_text = self.font.render("Start", True, WARM_SOFT_WHITE)
+        button_text_rect = button_text.get_rect(center=button_rect.center)
+        surface.blit(button_text, button_text_rect)
+
+    def check_start_button_click(self, mouse_position):
+        x, y = mouse_position
+        if((self.box_x_position <= x <= self.box_x_position + self.info_box_width) and (self.box_y_position <= y <= self.box_y_position + self.info_box_height)):
+            return True
+        return False
+    
